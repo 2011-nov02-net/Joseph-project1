133ea50467e93108c508c0940b305422e1b16118
Joseph Ellis <josephtomellis@gmail.com>
2020-12-07 23:12:06 -0600

    included some library testing

--- Project1/Project1.UnitTests/UnitTest1.cs
+++ Project1/Project1.UnitTests/UnitTest1.cs
@@ -1,4 +1,7 @@
+using Project1.Domain;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using Xunit;
 
 namespace Project1.UnitTests
@@ -8,10 +11,10 @@ namespace Project1.UnitTests
         [Fact]
         public void StoreConstructorTest()
         {
-            Product product2 = new Product("test product2", 5);
-            Product product1 = new Product("test product1", 2);
+            Product product2 = new Product("test product2", 5,10);
+            Product product1 = new Product("test product1", 2,12);
             List<Product> testInventory = new List<Product>() { product1, product2 };
-            Store testStore = new Store(testInventory, "123 Fake Street", "Test Store");
+            Store testStore = new Store(testInventory, "123 Fake Street", "Test Store",300);
 
             Assert.True(testStore.Inventory == testInventory);
             Assert.True(testStore.Name == "Test Store");
@@ -24,9 +27,9 @@ namespace Project1.UnitTests
         public void StoreAddItemTest(string itemName, int quantity)
         {
             List<Product> testInventory = new List<Product>();
-            Store testStore = new Store(testInventory, "123 Fake Street", "Test Store");
+            Store testStore = new Store(testInventory, "123 Fake Street", "Test Store",300);
 
-            testStore.AddItem(itemName, quantity);
+            testStore.AddItem(itemName, quantity,10);
 
             Assert.True(itemName == testStore.Inventory.First().Name);
         }
@@ -34,8 +37,8 @@ namespace Project1.UnitTests
         [Fact]
         public void StoreAddCustomerTest()
         {
-            Customer customer = new Customer("test Customer");
-            Store testStore = new Store("123 Fake Street", "Test Store");
+            Customer customer = new Customer("test",  "Customer",3);
+            Store testStore = new Store("123 Fake Street", "Test Store",300);
 
             testStore.AddCustomer(customer);
 
@@ -45,8 +48,8 @@ namespace Project1.UnitTests
         [Fact]
         public void StoreRemoveCustomerTest()
         {
-            Customer customer = new Customer("test Customer");
-            Store testStore = new Store("123 Fake Street", "Test Store")
+            Customer customer = new Customer("test", "Customer", 3);
+            Store testStore = new Store("123 Fake Street", "Test Store", 300)
             {
                 Customers = { customer }
             };
@@ -60,8 +63,8 @@ namespace Project1.UnitTests
         public void StoreRemoveItemTest()
         {
             string testItemName = "testItem";
-            Product product = new Product(testItemName, 2);
-            Store testStore = new Store("123 Fake Street", "Test Store")
+            Product product = new Product(testItemName, 2,10);
+            Store testStore = new Store("123 Fake Street", "Test Store",300)
             {
                 Inventory = { product }
             };
@@ -74,14 +77,14 @@ namespace Project1.UnitTests
         [Fact]
         public void StoreFillOrderTest()
         {
-            Product product1 = new Product("test product1", 2);
-            Customer testOrderer = new Customer("test orderer");
+            Product product1 = new Product("test product1", 2,5);
+            Customer testOrderer = new Customer("test" ,"orderer",5);
             List<Product> testSelections = new List<Product>() { product1 };
-            Store testStore = new Store("123 Fake Street", "Test Store")
+            Store testStore = new Store("123 Fake Street", "Test Store",300)
             {
-                Inventory = { new Product("test product1", 10) }
+                Inventory = { new Product("test product1", 10,12.99) }
             };
-            Order testOrder = new Order(testStore, testOrderer, testSelections);
+            Order testOrder = new Order(testStore, testOrderer, testSelections, 5);
 
             List<bool> ordersFilled = testStore.FillOrder(testOrder);
 
@@ -92,14 +95,14 @@ namespace Project1.UnitTests
         [Fact]
         public void StoreFillOrderTest2()//will fail
         {
-            Product product2 = new Product("test product2", 5);
-            Customer testOrderer = new Customer("test orderer");
+            Product product2 = new Product("test product2", 5,5);
+            Customer testOrderer = new Customer("test", "orderer", 5);
             List<Product> testSelections = new List<Product>() { product2 };
-            Store testStore = new Store("123 Fake Street", "Test Store")
+            Store testStore = new Store("123 Fake Street", "Test Store",300)
             {
-                Inventory = { new Product("test product1", 10) }
+                Inventory = { new Product("test product1", 10,12.99) }
             };
-            Order testOrder = new Order(testStore, testOrderer, testSelections);
+            Order testOrder = new Order(testStore, testOrderer, testSelections,5);
 
             List<bool> ordersFilled = testStore.FillOrder(testOrder);
 
@@ -108,53 +111,17 @@ namespace Project1.UnitTests
         }
 
         [Fact]
-        public void StoreRestockTest()
-        {
-            Product product2 = new Product("test product2", 5);
-            Product product1 = new Product("test product1", 2);
-            Customer testOrderer = new Customer("test orderer");
-            List<Product> testSelections = new List<Product>() { product1, product2 };
-            Store testStore = new Store("123 Fake Street", "Test Store")
-            {
-                Inventory = { new Product("test product1", 10) }
-            }; ;
-            Order testOrder = new Order(testStore, testOrderer, testSelections);
-
-            testStore.Restock(testOrder);
-
-            Assert.True(testStore.Inventory.First().Quantity > 10);
-        }
-
-        [Fact]
-        public void StoreRestockTest2()
-        {
-            Product product2 = new Product("test product2", 5);
-            Product product1 = new Product("test product1", 2);
-            Customer testOrderer = new Customer("test orderer");
-            List<Product> testSelections = new List<Product>();
-            Store testStore = new Store("123 Fake Street", "Test Store")
-            {
-                Inventory = { new Product("test product1", 10) }
-            }; ;
-            Order testOrder = new Order(testStore, testOrderer, testSelections);
-
-            testStore.Restock(testOrder);
-
-            Assert.True(testStore.Inventory.First().Quantity == 10);
-        }
-
-        [Fact]
         public void CustomerAddToOrderHistoryTest1()
         {
-            Project0.Library.Product product2 = new Project0.Library.Product("test product2", 5);
-            Project0.Library.Product product1 = new Project0.Library.Product("test product1", 2);
-            Project0.Library.Customer testOrderer = new Project0.Library.Customer("test orderer");
-            List<Project0.Library.Product> testSelections = new List<Project0.Library.Product>() { product1, product2 };
-            Project0.Library.Store testStore = new Project0.Library.Store("123 Fake Street", "Test Store")
+            Product product2 = new Product("test product2", 5,5.99);
+            Product product1 = new Product("test product1", 2,6.99);
+            Customer testOrderer = new Customer("test", "orderer", 5); ;
+            List<Product> testSelections = new List<Product>() { product1, product2 };
+            Store testStore = new Store("123 Fake Street", "Test Store",300)
             {
-                Inventory = { new Project0.Library.Product("test product1", 10) }
+                Inventory = { new Product("test product1", 10,7.99) }
             }; ;
-            Project0.Library.Order testOrder = new Project0.Library.Order(testStore, testOrderer, testSelections);
+            Order testOrder = new Order(testStore, testOrderer, testSelections, 5);
 
             testOrderer.AddToOrderHistory(testOrder);
 
